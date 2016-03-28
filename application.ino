2ada7786164157b66a7f18bdfa3ed924b7f240d4
Andrew <ajb2189@gmail.com>
2016-03-27 17:28:36 -0700

    initial commit

---
+++ application.ino
@@ -0,0 +1,12 @@
+
+//this is the fridge controller, programmed on 3/26 author Andrew Butterfield
+void setup()
+{
+	
+}
+
+//this is the loop that will run to do many things that manage the fridge
+void loop()
+{
+	
+}
